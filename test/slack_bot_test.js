0a1bc30be6badd3975731947ae07100dc56ff844
Zoee Silcock <mrzoee@gmail.com>
2015-06-15 12:36:39 +0200

    Connect to slack and identify our channel ID

--- test/slack_bot_test.js
+++ test/slack_bot_test.js
@@ -1,9 +1,40 @@
+var chai = require('chai');
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
 var expect = require('chai').expect;
+var Slack = require('slack-client');
 var SlackBot = require('../lib/slack_bot');
 
+chai.use(sinonChai);
+
 describe('SlackBot', function() {
-  it('can be instatiated', function() {
-    var bot = new SlackBot();
-    expect(bot).to.not.be.undefined;
+  before(function() {
+    sinon.stub(Slack.prototype, 'login');
+  });
+
+  beforeEach(function() {
+    this.bot = new SlackBot();
+  });
+
+  describe('constructor', function() {
+    it('can be instatiated', function() {
+      expect(this.bot).to.be.an.instanceof(SlackBot);
+    });
+
+    it('connects to the slack api', function() {
+      expect(this.bot.slack).to.be.an.instanceof(Slack);
+      expect(this.bot.slack.login).to.have.been.called;
+    });
+  });
+
+  describe('identifyChannel', function() {
+    it('extracts the channel ID from the slack data', function() {
+      var testData = {
+        'my super sweet test id': { name: 'mahvatar' },
+        'some other id': { name: 'who cares' }
+      };
+
+      expect(this.bot.identifyChannel(testData)).to.equal('my super sweet test id');
+    });
   });
 });
